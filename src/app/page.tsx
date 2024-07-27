d780d836de30fae8c9233338db06db8aeec030fe
Natwaa <Natwara_ch@cmu.ac.th>
2024-07-27 17:30:56 +0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,33 +10,40 @@ export default function Home() {
   // Define the interface of task-item object
   interface TaskItem {
     // your code here
+    id : string;
+    title : string;
+    completed : boolean;
+    deleteTaskFunc? : (taskId : string) => void;
+    toggleDoneTaskFunc? : (taskId : string) => void;
   }
 
   // useState hook for an array of task-item objects
   const [tasks, setTasks] = useState<TaskItem[]>([]);
 
   // Define the function with proper type
-  const addTask = (newTaskTitle) => {
+  const addTask = (newTaskTitle : string) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
-    setTasks(newTasks);
+      setTasks(newTasks);
   };
 
   // Define the function with proper type
-  const deleteTask = (taskId) => {
+  const deleteTask = (taskId : string) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   };
 
   // Define the function with proper type
-  const toggleDoneTask = (taskId) => {
+  const toggleDoneTask = (taskId : string) => {
     //structuredClone will copy an array or an object "deeply"
     //So objects within an object will be copied too
     const newTasks = structuredClone(tasks);
     //search for a task based on condition
     const task = newTasks.find((x) => x.id === taskId);
-    task.completed = !task.completed;
-    setTasks(newTasks);
+    if (task != undefined){
+      task.completed = !task.completed;
+      setTasks(newTasks);
+    }
   };
 
   return (
@@ -48,7 +55,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({tasks.length}) Done ({tasks.filter((x) => x.completed).length})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -67,7 +74,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2024" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2024" fullName="Natwara Chaiyasit" studentId="660610758" />
     </div>
   );
 }
